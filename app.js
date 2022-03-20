c71e51dd2b12e3f39dc28accaa630eec7aefcc8e
user2695 <zy.khan37@gmail.com>
2022-03-20 15:46:08 +0530

    app.js changes

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+ const firstName = req.body.fName;
+  const lastName = req.body.lName;
+ const email = req.body.email;
+
+  console.log(firstName );
+  res.sendFile(__dirname +"/success.html")
+});
+
+// app.get('/success', function (req, res) {
+//   res.sendFile(__dirname + '/success.html')
+// })
+// app.post('/signup', function (req, res) {
+//   console.log(req.body)
+//   res.redirect('/success')
+// })
+
+app.listen(3000, function () {
+  console.log("listening on 3000");
 })
-
-
-app.get('/success', function (req, res) {
-    res.sendFile(__dirname + '/success.html')
-  })
-  
-
-
-  app.post('/regist', function (req, res) {
-    console.log(req.body)
-    res.redirect('/success')
-  })
-
-
-
-
-
-
-
-
-app.post("/failure", function(req, res){
-   res.redirect("/")
-});
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-});
