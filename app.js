18812fd9baa25a38dae52c3e88d69a063c4d6c91
user2695 <zy.khan37@gmail.com>
2022-03-20 19:03:51 +0530

    added procfile

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
- const firstName = req.body.fName;
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
   const lastName = req.body.lName;
- const email = req.body.email;
+  const email = req.body.email;
 
-  console.log(firstName, lastName,email);
-  res.sendFile(__dirname +"/success.html")
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us14.api.mailchimp.com/3.0/lists/3dc7854b20";
+  const options = {
+    method: "POST",
+    auth: "zubi:0378d24a5e346b275ea9ba23165d4f2e-us14"
+  }
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+  console.log(firstName, lastName, email);
+  res.sendFile(__dirname + "/success.html")
 });
 
-
-// app.get('/success', function (req, res) {
-//   res.sendFile(__dirname + '/success.html')
-// })
-// app.post('/signup', function (req, res) {
-//   console.log(req.body)
-//   res.redirect('/success')
-// })
-
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("listening on 3000");
 })
+
+// 0378d24a5e346b275ea9ba23165d4f2e-us14
+
+// 3dc7854b20
